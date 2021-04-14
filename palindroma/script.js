2294cd0be59f3acc1336488b3b0bda639cc5d0b4
Cordovani <lor.cordovani@gmail.com>
2021-04-14 16:04:51 +0200

    struttura del codice e funzione di input

---
+++ palindroma/script.js
@@ -0,0 +1,19 @@
+// prendiamo la stringa di input dell'utente, facciamone una funzione riutilizzabile 
+function inputStringaPrompt() {
+    var input = prompt("si prega di inserire una stringa") 
+}
+
+console.log(input);
+
+// controllo della stringa d'input, ne farei una funzione 
+function isAstring(testo){
+
+} 
+
+
+// controlliamo se la stringa inserita è palindroma attraverso una funzione isPalindrome()
+
+// visualizziamo l'output in console stringa e se è palindroma
+
+
+
