840ca69b1601e61f370d258624415c38c00e873b
Cordovani <lor.cordovani@gmail.com>
2021-04-15 14:37:45 +0200

    stesura del programma e test

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -28,8 +28,30 @@ function isEven(possibileEven){
 //CORPO DEL PROGRAMMA
 
 // chiediamo l'input, un numero tra 1 e 5 compresi e "pari" o "dispari"
-// var numeroUtente = parseInt(prompt("inserire un numero tra 1 e 5"));
-// var pariOdispari = prompt("Vincerà pari o dispari?");
+var numeroUtente = parseInt(prompt("inserire un numero tra 1 e 5"));
+var pariOdispari = prompt("Vincerà pari o dispari?");
 
 // controlliamo l'input
+if()
+/* genera un numero random per il computer compreso tra 1 e 5 e lo somma a quello dell'utente */
+var numeroComputer = genRandomNumber(1,5);
+var somma = somma(numeroComputer,numeroUtente);
+
+// valutiamo se la somma è pari o dispari 
 // facciamo sapere chi è il vincitore
+if(isEven(somma) && pariOdispari === "pari"){
+    console.log("complimenti hai vinto");
+} else if(!isEven(somma) && pariOdispari === "dispari"){
+    console.log("complimenti hai vinto");
+} else {
+    console.log("mi dispiace ritenta");
+}
+
+// output per testare
+console.log("il numero immmesso è: " + numeroUtente);
+console.log("la puntata immessa è: " + pariOdispari);
+console.log("il numemro generato è: " + numeroComputer);
+console.log("la somma è: " + somma);
+console.log(isEven(somma));
+
+
