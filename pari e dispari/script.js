dc0bd1c23cf334156716c6927cdb6351dfa85090
Cordovani <lor.cordovani@gmail.com>
2021-04-15 14:26:58 +0200

    implementazione delle funzioni

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,9 +1,35 @@
+// FUNZIONI
+
+// la funzione genera un numero random tra 1 e 5 compresi
+function genRandomNumber(min,max){
+    // random() genera un numero tra 0 e 1
+    var numeroComputer = Math.floor(Math.random() * (max - min + 1) + min);
+    return numeroComputer;
+}
+
+// una funzione che esegue la somma di due numeri 
+function somma(num1,num2){
+    var totale = num1 + num2;
+    return totale;
+}
+
+// una funzione che determina se un numero è pari 
+function isEven(possibileEven){
+    var even = false;
+    if(possibileEven % 2 === 0){
+        even = true;
+    }
+    return even;
+}
+
+
+
+/****************************************************************************/
+//CORPO DEL PROGRAMMA
+
 // chiediamo l'input, un numero tra 1 e 5 compresi e "pari" o "dispari"
+// var numeroUtente = parseInt(prompt("inserire un numero tra 1 e 5"));
+// var pariOdispari = prompt("Vincerà pari o dispari?");
 
 // controlliamo l'input
-
-// generiamo un numero random tra 1 e 5 compreso per il pc con una funzione
-
-// calcoliamo il risultato attraverso una funzione
-
 // facciamo sapere chi è il vincitore
