66f5d3b9b338b2ff9e7a6de6c6c123e7003869a5
Cordovani <lor.cordovani@gmail.com>
2021-04-15 14:04:21 +0200

    struttura dell' programma

---
+++ pari e dispari/script.js
@@ -0,0 +1,9 @@
+// chiediamo l'input, un numero tra 1 e 5 compresi e "pari" o "dispari"
+
+// controlliamo l'input
+
+// generiamo un numero random tra 1 e 5 compreso per il pc con una funzione
+
+// calcoliamo il risultato attraverso una funzione
+
+// facciamo sapere chi è il vincitore
